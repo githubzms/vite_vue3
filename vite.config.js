cebea1472302b8fd8ebb3bcf715a9336ab7909b6
maczms <943844881@qq.com>
2023-12-11 15:59:01 +0800

    修改到github

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/vite_vue3',
+  base:'/vite_vue3/',
+  build:{
+    outDir:'docs'
+  },
   plugins: [vue()],
 })
